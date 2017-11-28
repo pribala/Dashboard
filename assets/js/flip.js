aee10404b7faf37a2c27538bc5676e2f843df660
Priya Balakrishnan <andu_pri@yahoo.com>
2017-11-27 23:44:42 -0500

    fix: Style project display text

--- assets/js/flip.js
+++ assets/js/flip.js
@@ -1,6 +1,14 @@
-$(".flip-container").hover(function(e){
-   	$(this).toggleClass("flipper");
-});
-$('.flip-container').on('touchstart', function(){
-	$(this).toggleClass("flipper");
+$('document').ready(function(){
+	$(".flip-container").hover(function(e){
+	   	$(this).toggleClass("flipper");
+	});
+
+	$('.flip-container').on('touchstart', function(){
+		$(this).toggleClass("flipper");
+	});
+
+	// $('.nav-link').click(function(){
+	// 	$('.nav-link').removeClass('active');
+	// 	$(this).addClass("active");
+	// });
 });
